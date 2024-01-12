663129f26a09194dbfaf16f8978b220e756e3421
Marcos Adriano <marcosadriano.ti@gmail.com>
2024-01-12 14:00:30 -0300

    Protected dashboard page with middleware

---
+++ middleware.ts
@@ -0,0 +1,9 @@
+import { withMiddlewareAuthRequired } from "@auth0/nextjs-auth0/edge";
+
+export default withMiddlewareAuthRequired();
+
+export const config = {
+  matcher: [
+    "/dashboard",
+  ],
+}
